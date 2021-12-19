f8f43360b0f69165d0bfa15e1587683f10724d3c
prabhjot <gchahal1970@gmail.com>
2021-12-19 10:08:15 -0800

    making it look little better

--- e_02_03/salarydivider.py
+++ e_02_03/salarydivider.py
@@ -1,10 +1,9 @@
 salary = input('how much is your salary?: ')
 salary = int(salary)
 print('your pay per hour is is $',(salary/52)/40)
-print('your pay per week is $',int(salary)/52)
-print('your pay biweekly is $',int(salary)/26)
-print('your pay per month is $',int(salary)/12)
-salary = int(salary)
+print('your pay per week is $',salary/52)
+print('your pay biweekly is $',salary/26)
+print('your pay per month is $',salary/12)
 if salary > 30000:
     if salary <=60000:
         print('you are doing great')

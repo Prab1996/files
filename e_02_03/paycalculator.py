5dda7b2d614ec0245b08cff149b4974042e01421
prabhjot <gchahal1970@gmail.com>
2021-12-19 12:30:27 -0800

    more id10 error removal

--- e_02_03/paycalculator.py
+++ e_02_03/paycalculator.py
@@ -4,10 +4,22 @@ try:
         x = 40
     if ft == 'no':
         x = input('how many hours you work per week?: ')
-    y = input('pay rate per hour?: ')
-    pay = int(x) * int(y)
-    print('your pay per week is $',pay)
-    print('your pay biweekly is $',pay*2)
-    print('your pay per year is $',pay*52)
+    try:
+        x = int(x)
+        x > 0
+        y = input('pay rate per hour?: ')
+        try:
+            y = int(y)
+            if y > 0:
+                pay = int(x) * int(y)
+                print('your pay per week is $',pay)
+                print('your pay biweekly is $',pay*2)
+                print('your pay per year is $',pay*52)
+            else:
+                print('payrate should be higher than 0, if it is not please call 911')
+        except:
+            print('please enter a number')
+    except:
+        print('enter your hours in a number higher than 0')
 except:
     print('please answer yes or no')

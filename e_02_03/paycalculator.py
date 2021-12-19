f2985d8ea5b8d8dd58450458fe58fcf0e18102a8
prabhjot <gchahal1970@gmail.com>
2021-12-19 11:51:27 -0800

    adding try statements for id10 error

--- e_02_03/paycalculator.py
+++ e_02_03/paycalculator.py
@@ -1,16 +1,13 @@
 ft = input('do you work fulltime?: ')
-if ft == 'yes':
-    x = 40
-if ft == 'no':
-    x = input('how many hours you work per week?: ')
-y = input('pay rate per hour?: ')
-pay = int(x) * int(y)
-print('your pay per week is $',pay)
-print('your pay biweekly is $',pay*2)
-print('your pay per year is $',pay*52)
-
-salary = input('how much is your salary?: ')
-print('your pay per hour is is $',(int(salary)/52)/40)
-print('your pay per week is $',int(salary)/52)
-print('your pay biweekly is $',int(salary)/26)
-print('your pay per month is $',int(salary)/12)
+try:
+    if ft == 'yes':
+        x = 40
+    if ft == 'no':
+        x = input('how many hours you work per week?: ')
+    y = input('pay rate per hour?: ')
+    pay = int(x) * int(y)
+    print('your pay per week is $',pay)
+    print('your pay biweekly is $',pay*2)
+    print('your pay per year is $',pay*52)
+else:
+    print('please answer yes or no')
--- e_02_03/salarydivider.py
+++ e_02_03/salarydivider.py
@@ -1,17 +1,23 @@
 salary = input('how much is your salary?: ')
-salary = int(salary)
-print('your pay per hour is is $',(salary/52)/40)
-print('your pay per week is $',salary/52)
-print('your pay biweekly is $',salary/26)
-print('your pay per month is $',salary/12)
-if salary > 30000:
-    if salary <=60000:
-        print('you are doing great')
-if salary > 60000:
-    if salary <=90000:
-        print('you are in the top 20% middle class')
-if salary > 90000 :
-    if salary <= 1000000 :
-        print('you are in top 1% of usa population')
-if salary > 1000000 :
-    print('you are in top 0.01% of usa population')
+try:
+    salary = int(salary)
+except:
+    salary = -1
+if salary > 0:
+    print('your pay per hour is is $',(salary/52)/40)
+    print('your pay per week is $',salary/52)
+    print('your pay biweekly is $',salary/26)
+    print('your pay per month is $',salary/12)
+    if salary > 30000:
+        if salary <=60000:
+            print('you are doing great')
+    if salary > 60000:
+        if salary <=90000:
+            print('you are in the top 20% middle class')
+    if salary > 90000 :
+        if salary <= 1000000 :
+            print('you are in top 1% of usa population')
+    if salary > 1000000 :
+        print('you are in top 0.01% of usa population')
+else:
+    print('salary need to be a number more than 0')

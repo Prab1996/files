068782536eb0cedd288a4ecb959d40ea4c75855b
prabhjot <gchahal1970@gmail.com>
2021-12-25 07:37:49 -0800

    learning to use quit()

--- e_02_03/overtimecalculator.py
+++ e_02_03/overtimecalculator.py
@@ -3,16 +3,16 @@ y = input ("how much is your payrate per hour?")
 try:
     x = int(x)
     y = int(y)
-    if x > 40:
-        ox = x - 40
-        rx = x - ox
-        oy = (y/2)+y
-        ot = ox*oy
-        print("your overtime pay is $", ot)
-        print("your regular pay is $", rx*y)
-        print("your total pay is $", (ot)+(rx*y))
-    else:
-        print("your total pay is $", (x*y))
-
 except:
     print("please enter hours and payrate as number greater than 0")
+    quit()
+if x > 40:
+    ox = x - 40
+    rx = x - ox
+    oy = (y/2)+y
+    ot = ox*oy
+    print("your overtime pay is $", ot)
+    print("your regular pay is $", rx*y)
+    print("your total pay is $", (ot)+(rx*y))
+else:
+    print("your total pay is $", (x*y))
